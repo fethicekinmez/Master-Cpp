e7b8c485a3655f488d5100315bde0178d49db0c3
Fethi Çekinmez <fethicekinmez@gmail.com>
2025-05-03 01:42:27 +0300

    Add main.cpp to demonstrate the use of logical operators with a beach day example

---
+++ 09-Logical_Operators/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main(){
+
+    bool isSunny = true;
+    bool isWeekend = true;
+
+    bool isBeachDay = isSunny && isWeekend; // Logical AND
+    std::cout << "Is it a beach day? " << (isBeachDay ? "Yes" : "No") << std::endl;
+
+    return 0;
+}
