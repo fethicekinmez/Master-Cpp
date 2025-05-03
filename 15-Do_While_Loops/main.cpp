25e5d59f33514e8c0f07f8bf73d7db1896a88b52
Fethi Çekinmez <fethicekinmez@gmail.com>
2025-05-03 12:21:01 +0300

    Add main.cpp to demonstrate the use of do-while loops for even and odd number identification

---
+++ 15-Do_While_Loops/main.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+
+int main(){
+
+    int num = 1;
+    do{
+        if(num % 2 == 0){
+            std::cout << num << " is even" << std::endl;
+            num++;
+            continue;
+        }
+        std::cout << num << " is odd" << std::endl;
+        num++;
+    }while(num <=20);
+
+    return 0;
+}
+/*
+1 is odd
+2 is even
+3 is odd
+4 is even
+5 is odd
+6 is even
+7 is odd
+8 is even
+9 is odd
+10 is even
+11 is odd
+12 is even
+13 is odd
+14 is even
+15 is odd
+16 is even
+17 is odd
+18 is even
+19 is odd
+20 is even
+*/
